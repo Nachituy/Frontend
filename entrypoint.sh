895fc36a70c6f77781fa367d5b25200086b0dcb0
GRoland-devops <127162715+GRoland-devops@users.noreply.github.com>
2023-07-13 20:51:28 -0300

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -40,7 +40,7 @@ aws configure --profile s3-sync-action <<-EOF > /dev/null 2>&1
 ${AWS_ACCESS_KEY_ID}
 ${AWS_SECRET_ACCESS_KEY}
 ${AWS_REGION}
-${aws-session-token}
+${AWS_SESSION_TOKEN}
 text
 EOF
 
