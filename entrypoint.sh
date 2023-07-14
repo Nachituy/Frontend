4deae3e50636dfbe0a839331c35e7aacf72d67c8
GRoland-devops <127162715+GRoland-devops@users.noreply.github.com>
2023-07-14 20:14:14 -0300

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -51,7 +51,8 @@ echo "aws_session_token=${AWS_SESSION_TOKEN}" >> ~/.aws/credentials
 
 # Sync using our dedicated profile and suppress verbose messages.
 # All other flags are optional via the `args:` directive.
-sh -c "aws s3 sync ${SOURCE_DIR:-.} s3://${AWS_S3_BUCKET}/${DEST_DIR} \
+# sh -c "aws s3 sync ${SOURCE_DIR:-.} s3://${AWS_S3_BUCKET}/${DEST_DIR} \
+sh -c "aws s3 sync  ./dist s3://${AWS_S3_BUCKET}/${DEST_DIR} \
               --profile s3-sync-action \
               --no-progress \
               ${ENDPOINT_APPEND} $*"
