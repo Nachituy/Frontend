eaf37152e6495044395e8b19769c935a97aad5a7
GRoland-devops <127162715+GRoland-devops@users.noreply.github.com>
2023-07-15 19:05:38 -0300

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -59,7 +59,7 @@ echo "aws_session_token=${AWS_SESSION_TOKEN}" >> ~/.aws/credentials
 sh -c "aws s3 sync ${SOURCE_DIR:-.} s3://${AWS_S3_BUCKET}/${DEST_DIR} \
               --profile s3-sync-action \
               --no-progress \
-              ${ENDPOINT_APPEND} --follow-symlinks --delete"
+              ${ENDPOINT_APPEND} $*"
 
 # Clear out credentials after we're done.
 # We need to re-run `aws configure` with bogus input instead of
