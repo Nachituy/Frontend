e809a1ed1a7d20bfee9e9b3589dc597992d9f750
GRoland-devops <127162715+GRoland-devops@users.noreply.github.com>
2023-07-14 21:41:04 -0300

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -52,10 +52,10 @@ echo "aws_session_token=${AWS_SESSION_TOKEN}" >> ~/.aws/credentials
 # Sync using our dedicated profile and suppress verbose messages.
 # All other flags are optional via the `args:` directive.
 # sh -c "aws s3 sync ${SOURCE_DIR:-.} s3://${AWS_S3_BUCKET}/${DEST_DIR} \
-sh -c "aws s3 sync  ./dist s3://${AWS_S3_BUCKET}/${DEST_DIR} \
+sh -c "aws s3 sync ${SOURCE_DIR:-.} s3://${AWS_S3_BUCKET}/${DEST_DIR} \
               --profile s3-sync-action \
-              --no-progress \ 
-              ${ENDPOINT_APPEND} --delete"
+              --no-progress \
+              ${ENDPOINT_APPEND} $*"
 
 # Clear out credentials after we're done.
 # We need to re-run `aws configure` with bogus input instead of
